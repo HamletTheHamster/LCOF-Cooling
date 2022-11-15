2c31be3d18a7aa91e29debdd0308c88bf2a33e80
Joel N. Johnson <joel.johnson675@gmail.com>
2022-11-15 11:07:12 -0700

    P-P return to normal

--- pumpProbePlotter.py
+++ pumpProbePlotter.py
@@ -28,7 +28,7 @@ if not os.path.exists(save):
 powers = ['0', '55', '110', '165']
 truePowers = []
 for pow in powers:
-    truePowers.append(float(int(pow) - 10)*.17**.5)
+    truePowers.append(float(int(pow))*.17**.5) # - 10 mW
 
 file = ['bas', 'ras', 'bs', 'rs']
 files = 5
